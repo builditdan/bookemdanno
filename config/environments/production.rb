146fc79000182238e68d0952f04692e312625311
builditdan <daniels111@gmail.com>
2016-01-27 17:55:15 -0500

    Updated mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     }
 
     #ActionMail config
-    config.action_mailer.default_url_options = { host: 'localhost', port:3000}
+    config.action_mailer.default_url_options = { host: 'https://bookemdanno.herokuapp.com/'}
     config.action_mailer.delivery_method = :smtp
     # change to true to allow email to be sent during development
     config.action_mailer.perform_deliveries = true
