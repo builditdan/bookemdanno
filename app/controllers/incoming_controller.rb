a94817f11902364aa1f95ef4658496dfe018c1ea
builditdan <daniels111@gmail.com>
2016-01-28 12:10:36 -0500

    fixes to incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,12 +9,12 @@ class IncomingController < ApplicationController
   def create
     puts "INCOMING PARAMS HERE: #{params}"
     logger.info "***********************************"
-    logger.info "body:#{params[:body-plain]}"
+    logger.info "body:#{params[:"body-plain"]}"
     logger.info "subject:#{params[:subject]}"
     logger.info "email:#{params[:sender]}"
     logger.info "***********************************"
 
-    incoming_urls = URI.extract(params[:body_plain])
+    incoming_urls = URI.extract(params[:"body_plain"])
     incoming_topic =  params[:subject]
     incoming_email =  params[:sender]
 
