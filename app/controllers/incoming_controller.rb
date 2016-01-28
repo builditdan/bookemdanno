2094ab280f4ad457ae8546b9e9bbc5d34732c6fd
builditdan <daniels111@gmail.com>
2016-01-28 13:55:11 -0500

    fixes to incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -22,6 +22,8 @@ class IncomingController < ApplicationController
     logger.info "Incoming subject: #{incoming_topic}"
     logger.info "Incoming urls: #{incoming_urls}"
 
+    incoming_urls.delete_if {|url| url.include? "wrote:" }
+
     user = User.find_by(email:incoming_email)
     if user.blank?
       logger.warn "Not a valid user: #{incoming_email}, exiting and not adding any bookmarks"
@@ -31,8 +33,7 @@ class IncomingController < ApplicationController
         #a_url = "FIX->#{a_url}" if !(a_url =~ URI::regexp)
         bookmark.url = a_url
         bookmark.topic_id = topic.id
-
-        if bookmark.save
+        if !(a_url.include? "wrote:") && bookmark.save
             logger.info "New bookmark created #{a_url}"
         else
             logger.warn "Unable to save bookmark #{a_url}, bookmark not stored, bummer!"
